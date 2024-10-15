77ea8afe149df81688db95e2ac99891e49629479
Qunthunnan <bredtv6@gmail.com>
2024-10-15 23:00:48 +0300

    Data imported

---
+++ dev-data/data/importTourData.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import mongoose from 'mongoose';
+import { Tour } from '../../models/tourModel';
+import { env } from '../../dotenv';
+
+if (env.parsed && 'DATABASE' in env.parsed && 'DB_PASSWORD' in env.parsed) {
+  const DB = env.parsed.DATABASE.replace(
+    '<db_password>',
+    env.parsed.DB_PASSWORD,
+  );
+
+  const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  );
+
+  mongoose
+    .connect(DB)
+    .then((con) => {
+      console.log('Mongo connectend!');
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+  if (process.argv.find((arg) => arg === '--delete')) {
+    Tour.deleteMany()
+      .then(() => {
+        console.log('Deleted!');
+        process.exit();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  } else {
+    Tour.create(tours)
+      .then(() => {
+        console.log('Imported!');
+        process.exit();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
+} else {
+  throw new Error('Environment config error');
+}
--- models/tourModel.ts
+++ models/tourModel.ts
@@ -5,14 +5,60 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Name is required'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5,
+    required: [true, 'Duration is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Group size is required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is required'],
+    trim: true,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Price is required'],
+  },
+  summary: {
+    type: String,
+    required: [true, 'Summary is required'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Image cover is required'],
+    trim: true,
+  },
+  images: {
+    type: [String],
+    required: [true, 'Images is required'],
+    trim: true,
+  },
+  startDates: {
+    type: [Date],
+    required: [true, 'Start dates is required'],
+  },
+  priceDiscount: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
